3cce82d4ebf4b9cbf526ab15bef5c9039839e309
SohyunWi <wsh@peopulley.com>
2022-01-08 22:38:30 +0900

    Initial commit

---
+++ src/main/java/com/example/hellospring/SpringConfig.java
@@ -0,0 +1,32 @@
+package com.example.hellospring;
+
+import com.example.hellospring.repository.JdbcMemberRepository;
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepository;
+import com.example.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+    
+    private DataSource dataSource;
+    
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+    
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JdbcMemberRepository(dataSource);
+//        return new MemoryMemberRepository();
+    
+    }
+}
---
+++ src/main/java/com/example/hellospring/service/MemberService.java
@@ -0,0 +1,47 @@
+package com.example.hellospring.service;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public class MemberService {
+    
+    private final MemberRepository memberRepository;
+    
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    
+    /* 회원 가입 */
+    public Long join(Member member) {
+    //    같은 이름이 있는 중복 회원 X
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+        validateDuplicateMember(member);
+    
+        memberRepository.save(member);
+        return member.getId();
+    }
+    
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+    
+    /* 전체 회원 조회 */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
